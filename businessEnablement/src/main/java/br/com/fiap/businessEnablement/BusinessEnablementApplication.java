7dad8c366951049cbd3a8b1902c3283814ff76fb
JhonnyO3 <jhonatas2004@gmail.com>
2023-03-17 11:39:43 -0300

    Projeto criado!

---
+++ businessEnablement/src/main/java/br/com/fiap/businessEnablement/BusinessEnablementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.businessEnablement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessEnablementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessEnablementApplication.class, args);
+	}
+
+}
---
+++ businessEnablement/src/test/java/br/com/fiap/businessEnablement/BusinessEnablementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.businessEnablement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessEnablementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
